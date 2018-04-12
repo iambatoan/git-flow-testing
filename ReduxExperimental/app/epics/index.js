70a529b46d6772217bb8cd44e03553264b450f22
Kha Tran <kha.tran@2359media.com>
2018-04-12 14:40:39 +0700

    [Network] Fix deprecate store.dispatch in epic
    - Move navigate function to nav reducer

--- ReduxExperimental/app/epics/index.js
+++ ReduxExperimental/app/epics/index.js
@@ -1,37 +1,25 @@
+import 'rxjs';
 import { combineEpics } from 'redux-observable';
-import { NavigationActions } from 'react-navigation';
-import { Observable } from 'rxjs';
 import { ActionTypes } from '../actions';
 
 import API from '../api';
-import { StringConfig } from '../config';
 
 const onFetchOfferList = (action$, store) =>
   action$
     .ofType(ActionTypes.FETCH_OFFER)
     .switchMap(action => API.getOfferList())
-    .mergeMap(resp => {
-      store.dispatch(
-        NavigationActions.navigate({
-          routeName: StringConfig.OfferList.Name,
-          params: { datas: resp.data.offers }
-        })
-      );
-      return Observable.never();
-    });
+    .map(resp => ({
+      type: ActionTypes.NAVIGATE_OFFER,
+      data: resp.data.offers
+    }));
 
 const onFetchDetailOfffer = (action$, store) =>
   action$
     .ofType(ActionTypes.FETCH_DETAIL_OFFER)
     .switchMap(action => API.getDetailOffer(action.id))
-    .mergeMap(resp => {
-      store.dispatch(
-        NavigationActions.navigate({
-          routeName: StringConfig.DetailOffer.Name,
-          params: { data: resp.data }
-        })
-      );
-      return Observable.never();
-    });
+    .map(resp => ({
+      type: ActionTypes.NAVIGATE_DETAIL_OFFER,
+      data: resp.data
+    }));
 
 export default combineEpics(onFetchOfferList, onFetchDetailOfffer);
--- ReduxExperimental/app/reducer/nav.js
+++ ReduxExperimental/app/reducer/nav.js
@@ -1,11 +1,33 @@
+import { NavigationActions } from 'react-navigation';
 import { AppNavigator } from '../navigator';
 import { StringConfig } from '../config';
+import { ActionTypes } from '../actions';
 
 const initialState = AppNavigator.router.getStateForAction(
   AppNavigator.router.getActionForPathAndParams(StringConfig.Home.Name)
 );
 
 export default (state = initialState, action = {}) => {
-  const nextState = AppNavigator.router.getStateForAction(action, state);
+  let nextState = AppNavigator.router.getStateForAction(action, state);
+  switch (action.type) {
+    case ActionTypes.NAVIGATE_OFFER:
+      nextState = AppNavigator.router.getStateForAction(
+        NavigationActions.navigate({
+          routeName: StringConfig.OfferList.Name,
+          params: { datas: action.data }
+        }),
+        state
+      );
+      break;
+    case ActionTypes.NAVIGATE_DETAIL_OFFER:
+      nextState = AppNavigator.router.getStateForAction(
+        NavigationActions.navigate({
+          routeName: StringConfig.DetailOffer.Name,
+          params: { data: action.data }
+        }),
+        state
+      );
+      break;
+  }
   return nextState || state;
 };
